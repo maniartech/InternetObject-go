efbadf554d7c9c67e77cd2e8727bb3f030a93f04
Aamir Maniar <aamir.maniar@maniartech.com>
2020-06-17 11:53:44 +0530

    Initial setup

---
+++ parsers/constants.go
@@ -0,0 +1,25 @@
+package parsers
+
+// Comma represents the comma , character
+const Comma string = ","
+
+// Colon represents the colon : character
+const Colon string = ":"
+
+// OpenCurly represents the open curly bracket { character
+const OpenCurly string = "{"
+
+// CloseCurly represents the close curly bracket } character
+const CloseCurly string = "}"
+
+// OpenSquare represents the open square bracket [ character
+const OpenSquare string = "["
+
+// CloseSquare represents the close square bracket ] character
+const CloseSquare string = "]"
+
+// Tilde represents the tilde ~ character
+const Tilde string = "~"
+
+// Datasep represents the --- char sequence
+const Datasep string = "---"
