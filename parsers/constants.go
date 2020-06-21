2961780f3d67d40e2e0d56fd6fadecedaae5b6d3
Aamir Maniar <aamir.maniar@maniartech.com>
2020-06-21 22:02:30 +0530

    Constants updated

--- parsers/constants.go
+++ parsers/constants.go
@@ -1,25 +1,48 @@
 package parsers
 
 // Comma represents the comma , character
-const Comma string = ","
+const Comma = ','
 
 // Colon represents the colon : character
-const Colon string = ":"
+const Colon = ':'
 
 // OpenCurly represents the open curly bracket { character
-const OpenCurly string = "{"
+const OpenCurly = '{'
 
 // CloseCurly represents the close curly bracket } character
-const CloseCurly string = "}"
+const CloseCurly = '}'
 
 // OpenSquare represents the open square bracket [ character
-const OpenSquare string = "["
+const OpenSquare = '['
 
 // CloseSquare represents the close square bracket ] character
-const CloseSquare string = "]"
+const CloseSquare = ']'
 
 // Tilde represents the tilde ~ character
-const Tilde string = "~"
+const Tilde = '~'
 
 // Datasep represents the --- char sequence
-const Datasep string = "---"
+const Datasep = "---"
+
+// Space represents the space character
+const Space = ' '
+
+// NewLine represents the newlne \n character
+const NewLine = '\n'
+
+// CarrigeReturn represents the carriege return \r character
+const CarrigeReturn = '\r'
+
+// Hash represent the hash # character
+const Hash = '#'
+
+// Separators represents string of separators
+const Separators = "{:}[,]"
+
+// **** Types ********************************************************
+
+// TypeSeparator represents the separator type
+const TypeSeparator = "sep"
+
+// TypeString represents the separator type
+const TypeString = "string"
