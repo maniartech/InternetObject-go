ac694104443a7c2fa15d033786156e0963457668
Aamir Maniar <aamir.maniar@maniartech.com>
2020-06-21 22:03:22 +0530

    Utility setup

---
+++ utils/printing.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+// PrettyPrint prints the formatted output for the specified struct
+func PrettyPrint(val interface{}) {
+	o, e := json.MarshalIndent(val, "", "  ")
+	if e != nil {
+		log.Panic(e.Error())
+	}
+	fmt.Printf(string(o))
+	fmt.Println()
+}
